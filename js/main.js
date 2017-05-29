d1290c8fec99244f72200a0980d929afbf7ffabe
Billy Franz <billfranz00@gmail.com>
2017-05-28 23:42:18 -0700

    Starting to build sorting functions

--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
 $(document).ready(function() {
-	var $mainButton = $('#mainButton');
+	var $mainButton = $('#mainButton'),
+		$placeUp = $('#placeUp'),
+		$placeDown = $('#placeDown'),
+		$ratingUp = $('#ratingUp'),
+		$ratingDown = $('#ratingDown');
 
 	$mainButton.on('click', function() {
 		var $place = $('<td>', {
 			text: $('#place').val()
-		}),
+		}).addClass('place'),
 		$rating = $('<td>',  {
 			text: $('#rating').val()
-		}),
+		}).addClass('rating'),
 		$deleteButton = $('<td>')
 			.append($('<button>', {
 				text: 'Delete',
 				type: 'button'})
 			.addClass('btn btn-danger')
-			.on('click', function(e) {
+			.on('click', function(e) { // event which removes row in which delete button is located
 				$(e.target)
 				.parent()
 				.parent()
 				.remove()
-			})),
+			})
+		),
 		$tr = $('<tr>')
 			.append($place)
 			.append($rating)
 			.append($deleteButton);
 		$('tbody').append($tr);
 		console.log($('#place').val())
+	})
+
+	$placeUp.on('click', function() {
+		var placeSort = $('tbody tr').map(function(i, el) {
+			return $(el).find('.place').text()
+		})
+		console.log(placeSort)
+		placeSort.sort()
+		console.log(placeSort)
 	})
 
 })
